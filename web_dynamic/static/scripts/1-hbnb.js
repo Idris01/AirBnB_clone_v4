2efc464cb25ad9ddf1dcfd0d2ecf13809e048af5
Idris Adebowale <idrys01@gmail.com>
2023-10-06 15:57:58 +0000

    fix: template file, update checkbox spacing and clean unwanted files

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-  let amenities = {};
+  const amenities = {};
   $(document).on('change', "input[type='checkbox']", function () {
     if (this.checked) {
       amenities[$(this).data('id')] = $(this).data('name');
     } else {
       delete amenities[$(this).data('id')];
     }
-    let res = Object.values(amenities);
+    const res = Object.values(amenities);
     if (res.length > 0) {
       $('div.amenities > h4').text(Object.values(amenities).join(', '));
     } else {
